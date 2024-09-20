1fe4b8d8120e25852774445f8152b371883325f5
Jea-Eok-Kim <je.kim@xiilab.com>
2024-09-20 16:47:25 +0900

    애니메이션 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,24 +2,93 @@ import React from "react";
 import * as Style from "./styles";
 import Header from "compoents/header";
 
+type ACTION_TYPE = "action" | "nomal";
+
 function App() {
+  const [onLeft, setOnLeft] = React.useState<ACTION_TYPE>("action");
+  const [onRight, setOnRight] = React.useState<ACTION_TYPE>("action");
+
+  /**
+   * 마우스 커서가 위치 할때 이벤트 처리
+   * @param e
+   */
+  const sizeUp = (e: React.MouseEvent<HTMLDivElement>) => {
+    const name = e.currentTarget.attributes.getNamedItem("data-cy")?.value;
+    if (name === "left") {
+      setOnLeft("action");
+    } else {
+      setOnRight("action");
+    }
+    // console.log(e);
+  };
+  /**
+   * 마우스 커서가 나갈때 이벤트 처리
+   * @param e
+   */
+  const sizeDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    const name = e.currentTarget.attributes.getNamedItem("data-cy")?.value;
+    if (name === "left") {
+      setOnLeft("nomal");
+    } else {
+      setOnRight("nomal");
+    }
+    // console.log(e);
+  };
   return (
     <>
       <Header data-cy="Header" />
       <Style.Body data-cy="Body">
-        <Style.Left data-cy="Left">
+        <Style.Left
+          data-cy="left"
+          className={onLeft}
+          onMouseEnter={sizeUp}
+          onMouseLeave={sizeDown}
+        >
           <h1 className="title">Cloud Server</h1>
           <h3 className="description">
             사용자 목적에 맞는 사양의 가상 서버 생성
           </h3>
+          <div className={`infomation ${onLeft}`}>
+            <div className="item">
+              <h3>제공 OS</h3>
+              <p>Linux 계열 서버 시스템</p>
+            </div>
+            <div className="item">
+              <h3>제공 목적</h3>
+              <p>SW 개발 및 서비스 제공</p>
+            </div>
+            <div className="item">
+              <h3>사용자 환경</h3>
+              <p>CLI 기반 키보드 입력</p>
+            </div>
+          </div>
           <button>Server 바로가기</button>
         </Style.Left>
-        <Style.Right data-cy="Right">
+        <Style.Right
+          data-cy="right"
+          className={onRight}
+          onMouseEnter={sizeUp}
+          onMouseLeave={sizeDown}
+        >
           <h1 className="title">Cloud PC</h1>
           <h3 className="description">
             국가 보안 요구사항을 준수하는 높은 보안성
           </h3>
           <p>(국가정보원 보안기능확인서 획득, 2022.06월)</p>
+          <div className={`infomation ${onRight}`}>
+            <div className="item">
+              <h3>제공 OS</h3>
+              <p>Linux 계열 서버 시스템</p>
+            </div>
+            <div className="item">
+              <h3>제공 목적</h3>
+              <p>시뮬레이션 및 데이터 분석</p>
+            </div>
+            <div className="item">
+              <h3>사용자 환경</h3>
+              <p>GUI기반 마우스, 키보드 입력</p>
+            </div>
+          </div>
           <button>PC 바로가기</button>
         </Style.Right>
       </Style.Body>
--- src/pages/main/styles.ts
+++ src/pages/main/styles.ts
@@ -3,9 +3,72 @@ import leht from "./left-bg.png";
 import right from "./right-bg.png";
 import icon from "./button.svg";
 
+import lehtFull from "./left-bg-full.png";
+import rightFull from "./right-bg-full.png";
+
+import { keyframes } from "@emotion/react";
+
+const boxWidth = keyframes`
+  0% {
+    /* width: 0%; */
+    width: 1280px;
+  }
+  100% {
+    ${tw`w-full`}
+  }
+`;
+const boxHeight = keyframes`
+  0% {
+    /* width: 0%; */
+    height: 0px;
+  }
+  100% {
+    height: 95px;
+  }
+`;
+
 export const Body = styled("div")`
   ${tw`flex flex-row w-full h-full overflow-hidden`}
   position: relative;
+  div.infomation {
+    display: flex;
+    flex-direction: row;
+    padding: 20px 21px 20px 21px;
+    gap: 39px;
+    width: 300px;
+    height: 95px;
+    overflow-y: hidden;
+    &.nomal {
+      display: none;
+    }
+    &.action {
+      animation: ${boxHeight} 0.5s;
+    }
+    div.item {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      h3 {
+        font-family: NotoSans;
+        font-size: 12px;
+        font-weight: bold;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 1.67;
+        letter-spacing: normal;
+      }
+      p {
+        font-family: NotoSans;
+        font-size: 12px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 1.33;
+        letter-spacing: normal;
+      }
+    }
+  }
   h1.title {
     font-family: NotoSans;
     font-size: 44px;
@@ -27,7 +90,6 @@ export const Body = styled("div")`
     margin-bottom: 2px;
   }
   p {
-    opacity: 0.8;
     font-family: NotoSans;
     font-size: 12px;
     font-weight: normal;
@@ -36,7 +98,6 @@ export const Body = styled("div")`
     line-height: normal;
     letter-spacing: -0.12px;
     text-align: center;
-    color: #d9daeb;
   }
   button {
     width: 170px;
@@ -68,24 +129,56 @@ export const Body = styled("div")`
 `;
 
 export const Left = styled("div")`
-  ${tw`flex w-full h-full flex-col items-center text-marine`}
+  ${tw`flex h-full flex-col items-center text-marine`}
   background-image: url(${leht});
+  /* 액션 이벤트 동작시 처리 */
+  &.nomal {
+    ${tw`w-1/2`}
+  }
+  &.action {
+    ${tw`w-full`}
+    animation: ${boxWidth} 0.5s;
+    background-image: url(${lehtFull});
+  }
   background-repeat: no-repeat;
-  background-position: center center;
+  background-position: left 100% center;
   background-size: cover;
   padding-top: 300px;
   button {
     margin-top: 40px;
   }
+  div.infomation {
+    margin-top: 40px;
+    border-top: 1px solid rgba(0, 30, 96, 0.3);
+    border-bottom: 1px solid rgba(0, 30, 96, 0.3);
+  }
 `;
 export const Right = styled("div")`
-  ${tw`flex w-full h-full flex-col items-center text-white`}
+  ${tw`flex w-1/2 h-full flex-col items-center text-white`}
   background-image: url(${right});
+  &.nomal {
+    ${tw`w-1/2`}
+  }
+  /* 액션 이벤트 동작시 처리 */
+  &.action {
+    ${tw`w-full`}
+    animation: ${boxWidth} 0.5s;
+    background-image: url(${rightFull});
+  }
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
   padding-top: 300px;
   button {
     margin-top: 31px;
+  }
+  div.infomation {
+    margin-top: 20px;
+    border-top: 1px solid rgba(255, 255, 255, 0.4);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.4);
+  }
+  p {
+    opacity: 0.8;
+    color: #d9daeb;
   }
 `;
